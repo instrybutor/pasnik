234d6b0a1b6760d1c6eb462eb5d40b90e9453ca2
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2021-08-06 20:35:05 +0200

    WIP: Auth

---
+++ apps/frontend/src/app/SignIn.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import GoogleLogin from 'react-google-login';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Paśnik
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(20),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <GoogleLogin
+          className={classes.submit}
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+      <Box mt={3}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
