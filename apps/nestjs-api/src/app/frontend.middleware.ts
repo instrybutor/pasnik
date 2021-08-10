8a363644e3ec6ea5dc640d00a301034bbc3480c8
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2021-08-10 10:40:56 +0200

    Frontend middleware

--- apps/nestjs-api/src/app/frontend.middleware.ts
+++ apps/nestjs-api/src/app/frontend.middleware.ts
@@ -1,13 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class FrontendMiddleware implements NestMiddleware {
-  use(req, res, next) {
-    const { url } = req;
-    if (url.indexOf('/api') === 1) {
-      next();
-    } else {
+  use(req: Request, res: Response, next: NextFunction) {
+    if (environment.production) {
       res.sendFile('/public/index.html');
+    } else {
+      next();
     }
   }
 }
