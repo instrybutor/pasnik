5930333a4b95ab3fa3b8c7adb53e43a480546151
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-22 10:43:01 +0200

    create new component OrderElement

--- apps/frontend/src/app/Dashboard/Dashboard.tsx
+++ apps/frontend/src/app/Dashboard/Dashboard.tsx
@@ -1,8 +1,9 @@
 import { Fragment, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-
+import Grid from '@mui/material/Grid';
 import { OrderModel, OrderStatus } from '@pasnik/api/data-transfer';
 import { useOrdersFacade } from '@pasnik/orders-data-access';
+import OrderElement from '../OrderElement';
 
 export default function Dashboard() {
   const [orders, setOrders] = useState<OrderModel[]>([]);
@@ -28,25 +29,7 @@ export default function Dashboard() {
       </button>
       <div className="grid grid-flow-row grid-cols-12 grid-rows-3 gap-1 my-2">
         {orders.map((order, index) => (
-          <Fragment key={order.id}>
-            <div className="bg-gray-100 col-span-1 p-2 rounded">
-              {index + 1}
-            </div>
-            <div className="bg-gray-100 col-span-2 p-2 rounded">
-              <Link to={`/order/${order.id}`}>{order.from}</Link>
-            </div>
-            <div className="bg-gray-100 col-span-5 p-2 rounded">
-              {order.menuUrl}
-            </div>
-            <div className="bg-gray-100 col-span-2 p-2 rounded">
-              {order.createdAt}
-            </div>
-            <div className="bg-gray-100 col-span-2 p-2 rounded">
-              <span className="bg-green-300 text-white text-xs p-2 py-1 rounded">
-                {OrderStatus[order.status]}
-              </span>
-            </div>
-          </Fragment>
+          <OrderElement order = {order} index = {index}></OrderElement>
         ))}
       </div>
     </div>
