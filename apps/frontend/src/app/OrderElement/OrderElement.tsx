01e2d91a9be554afacae0b3fa70436fc3cc5d27d
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-23 21:03:59 +0200

    fixing code after cr

--- apps/frontend/src/app/OrderElement/OrderElement.tsx
+++ apps/frontend/src/app/OrderElement/OrderElement.tsx
@@ -1,73 +1,57 @@
 import { Grid, styled, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { OrderModel, OrderStatus } from '@pasnik/api/data-transfer';
-import { Fragment } from 'react';
+import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function OrderElement(props: {
-  order: OrderModel;
-  index: number;
-}) {
-  const { order, index } = props;
-  const Tile = styled(Box)(({ theme }) => ({
-    ...theme.typography.body2,
-    elevation: 24,
-  }));
-  const routeChange = (menuUrl: string) => {
-    window.open(menuUrl, '_blank');
-  };
+const StyledTextField = styled(TextField)`
+  .MuiOutlinedInput-input {
+    cursor: pointer;
+  }
+`;
 
-  const StyledTextField = styled(TextField)`
-    .MuiOutlinedInput-input {
-      cursor: pointer;
-    }
-  `;
+export default function OrderElement(props: { order: OrderModel }) {
+  const order = props.order;
+  const statusColor =
+    order.status === 0 ? 'primary' : order.status === 1 ? 'warning' : 'success';
+  const redirectToLink = useCallback(
+    (event) => {
+      event.stopPropagation();
+      window.open(order.menuUrl, '_blank');
+    },
+    [order.menuUrl]
+  );
   return (
-    <Fragment key={index}>
-      <Grid container direction="row">
-        <Grid item xs={9} direction="column">
-          <Tile>
-            <Typography variant="h5" color="initial" display="block">
-              <Link to={`/order/${order.id}`}>{order.from}</Link>
-            </Typography>
-            <Typography variant="h6" color="initial" display="block">
-              <span
-                onClick={(event) => {
-                  event.stopPropagation();
-                  return routeChange(order.menuUrl);
-                }}
-              >
-                {order.menuUrl}
-              </span>
-            </Typography>
-            <Typography variant="overline" color="initial" display="block">
-              {order.user} User is empty (bug)
-            </Typography>
-          </Tile>
-        </Grid>
-        <Grid item xs={3} direction="column">
-          <Box>
-            <StyledTextField
-              inputProps={{
-                readOnly: true,
-              }}
-              label="ORDER STATUS"
-              color={
-                order.status === 0
-                  ? 'primary'
-                  : order.status === 1
-                  ? 'warning'
-                  : 'success'
-              }
-              focused
-              defaultValue={OrderStatus[order.status]}
-            />
-            <Typography variant="overline" color="initial" display="block">
-              Created: {order.createdAt}
-            </Typography>
-          </Box>
-        </Grid>
+    <Grid container direction="row">
+      <Grid item xs={9} direction="column">
+        <Typography variant="h5" color="initial" display="block">
+          <Link to={`/order/${order.id}`}>{order.from}</Link>
+        </Typography>
+        <Typography variant="h6" color="initial" display="block">
+          <span onClick={(event) => redirectToLink(event)}>
+            {order.menuUrl}
+          </span>
+        </Typography>
+        <Typography variant="overline" color="initial" display="block">
+          {order.user ? order.user : 'User is not declared (bug)'}
+        </Typography>
       </Grid>
-    </Fragment>
+      <Grid item xs={3} direction="column">
+        <Box>
+          <StyledTextField
+            inputProps={{
+              readOnly: true,
+            }}
+            label="ORDER STATUS"
+            color={statusColor}
+            focused
+            defaultValue={OrderStatus[order.status]}
+          />
+          <Typography variant="overline" color="initial" display="block">
+            Created: {order.createdAt}
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
--- apps/frontend/src/app/OrderList/OrderList.tsx
+++ apps/frontend/src/app/OrderList/OrderList.tsx
@@ -1,37 +1,37 @@
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, styled } from '@mui/material';
 import { OrderModel } from '@pasnik/api/data-transfer';
-import { Fragment } from 'react';
+import { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import OrderElement from '../OrderElement';
 
 export default function OrderList(props: { orders: OrderModel[] }) {
   const history = useHistory();
 
-  const routeChange = (id: string) => {
-    history.push(`/order/${id}`);
-  };
+  const StyledCardActionArea = styled(CardActionArea)(() => ({
+    margin: 8,
+    padding: 8,
+    border: '1px solid black',
+    borderRadius: 10,
+    boxShadow: 'inset 0 7px 30px -10px rgba(150,170,180,0.5)',
+  }));
+
+  const routeChange = useCallback(
+    (id: string) => {
+      history.push(`/order/${id}`);
+    },
+    [history]
+  );
+
   return (
-    <div>
-      {props.orders.map((order, index) => (
-        <Fragment key={order.id}>
-          <CardActionArea
-            className="raise"
-            style={{
-              margin: 8,
-              padding: 8,
-              border: '1px solid black',
-              borderRadius: 10,
-              boxShadow: 'inset 0 7px 30px -10px rgba(150,170,180,0.5)',
-            }}
-            onClick={(event) => {
-              event.preventDefault();
-              return routeChange(order.id);
-            }}
-          >
-            <OrderElement order={order} index={index}></OrderElement>
-          </CardActionArea>
-        </Fragment>
+    <>
+      {props.orders.map((order) => (
+        <StyledCardActionArea
+          className="raise"
+          onClick={() => routeChange(order.id)}
+        >
+          <OrderElement order={order}></OrderElement>
+        </StyledCardActionArea>
       ))}
-    </div>
+    </>
   );
 }
