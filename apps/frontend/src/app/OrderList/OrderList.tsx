8b13fa43b4de4423dd303ee8043157ce77650a22
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-24 12:53:00 +0200

    resolved problems with conflict

--- apps/frontend/src/app/OrderList/OrderList.tsx
+++ apps/frontend/src/app/OrderList/OrderList.tsx
@@ -1,12 +1,11 @@
 import { CardActionArea, styled } from '@mui/material';
 import { OrderModel } from '@pasnik/api/data-transfer';
-import { MouseEventHandler, useCallback } from 'react';
+import { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import OrderElement from '../OrderElement';
 
 export default function OrderList(props: { orders: OrderModel[] }) {
   const history = useHistory();
-
   const StyledCardActionArea = styled(CardActionArea)(() => ({
     margin: 8,
     padding: 8,
