ec58b692dd6f95e89ca692ea7f0cd30e7831b9a2
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-27 11:21:41 +0200

    added create recipe form and validation

--- apps/frontend/src/app/CreateOrder/CreateOrder.tsx
+++ apps/frontend/src/app/CreateOrder/CreateOrder.tsx
@@ -4,8 +4,7 @@ import { useHistory } from 'react-router-dom';
 
 import type { CreateOrderDto, OrderModel } from '@pasnik/api/data-transfer';
 import { useOrdersFacade } from '@pasnik/orders-data-access';
-import { Button, TextField } from '@mui/material';
-import CurrencyTextField from '@unicef/material-ui-currency-textfield';
+import { Alert, Button, InputAdornment, Paper, TextField } from '@mui/material';
 
 export const CreateOrder: FC = () => {
   const { register, handleSubmit } = useForm();
@@ -15,31 +14,44 @@ export const CreateOrder: FC = () => {
   const [menu, setMenu] = useState<string>('');
   const [isValid, setIsValid] = useState<boolean>(false);
   const [isValidInput, setIsValidInput] = useState<boolean>(false);
-  const [DeliveryPrice, setDeliveryPrice] = useState<number>(0);
+  const [DeliveryPrice, setDeliveryPrice] = useState<string>('');
 
   const [error, setError] = useState<string | null>(null);
   const history = useHistory();
 
   const onSubmit = (data: CreateOrderDto) => {
-    createOrder({ ...data, orderAt: new Date().toISOString() })
-      .then((params: OrderModel) => history.push(`/`))
+    const newPrice =
+      typeof data.shippingCents === 'string'
+        ? data.shippingCents.replace(/,|\./, '')
+        : '';
+    createOrder({
+      ...data,
+      orderAt: new Date().toISOString(),
+      shippingCents: +newPrice,
+    })
+      .then((params: OrderModel) => history.push(`order/` + params.id))
       .catch((err: Error) => setError(err.message));
   };
 
-  const clearError = () => {
-    setError(null);
-  };
-
   function handleChangeRestaurant(e: ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
     setRestaurant(e.currentTarget.value);
   }
 
   function handleChangeMenu(e: ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
     setMenu(e.currentTarget.value);
     setIsValid(validUrl(e.currentTarget.value));
     setIsValidInput(validUrl(e.currentTarget.value));
+  }
+
+  function handleChangeDeliveryPrice(e: ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.currentTarget.value.replace(/[^0-9.,]/g, '');
+    const priceRefactor = inputValue.split(/,|\./, 2);
+    let price = priceRefactor[0];
+    if (priceRefactor[1] === '' || priceRefactor[1]) {
+      const secondPricePart = ',' + priceRefactor[1].replace(/,|\./, '');
+      price += secondPricePart.replace(/(?<=[0-9]{2}).+/, '');
+    }
+    setDeliveryPrice(price);
   }
 
   function validUrl(text: string) {
@@ -52,76 +64,66 @@ export const CreateOrder: FC = () => {
 
   return (
     <div>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          label="Restaurant"
-          id="email"
-          value={restaurant}
-          inputProps={{
-            fullWidth: true,
-          }}
-          onChange={handleChangeRestaurant}
-          type="text"
-        />
-        &nbsp;
-        <TextField
-          error={isValidInput}
-          label="Menu URL"
-          id="password"
-          value={menu}
-          inputProps={{
-            fullWidth: true,
-          }}
-          onChange={handleChangeMenu}
-          type="text"
-        />
-        &nbsp;
-        <CurrencyTextField
-          label="Amount"
-          variant="standard"
-          // value={value}
-          currencySymbol="$"
-          //minimumValue="0"
-          outputFormat="string"
-          decimalCharacter="."
-          digitGroupSeparator=","
-          //  onChange={(event, value) => setValue(value)}
-        />
-        <Button
-          type="submit"
-          color="success"
-          disabled={isValid}
-          variant="outlined"
-        >
-          Confirm
-        </Button>
-      </form>
-    </div>
+      <Paper elevation={3} className="formSquare">
+        {error && <Alert severity="error">Request Failed!</Alert>}
 
-    // <div className="bg-gray-100 w-full h-screen">
-    //   <div className="container flex justify-center">
-    //     <div className=" bg-white p-6 rounded">
-    //       <div>{error}</div>
-    //       <form onSubmit={handleSubmit(onSubmit)}>
-    //         <input
-    //           className="w-full p-4 border border-gray-400 rounded mb-2"
-    //           {...register('from')}
-    //           placeholder="Restaurant name"
-    //         />
-    //         <input
-    //           className="w-full p-4 border border-gray-400 rounded mb-2"
-    //           {...register('menuUrl')}
-    //           placeholder="Menu URL"
-    //         />
-    //         <button
-    //           className="p-4 px-6 bg-blue-400 text-white rounded"
-    //           type="submit"
-    //         >
-    //           Create order
-    //         </button>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            margin="normal"
+            label="Restaurant"
+            id="email"
+            value={restaurant}
+            inputProps={{
+              fullWidth: true,
+            }}
+            {...register('from')}
+            onChange={handleChangeRestaurant}
+            type="text"
+            required
+          />
+          <TextField
+            margin="normal"
+            error={isValidInput}
+            label="Menu URL"
+            id="password"
+            value={menu}
+            inputProps={{
+              fullWidth: true,
+            }}
+            {...register('menuUrl')}
+            onChange={handleChangeMenu}
+            type="text"
+            required
+          />
+          {isValidInput && (
+            <Alert severity="error">This URL is not valid!</Alert>
+          )}
+
+          <TextField
+            margin="normal"
+            label="Delivery Price (optional)"
+            id="outlined-start-adornment"
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">PLN</InputAdornment>
+              ),
+            }}
+            value={DeliveryPrice}
+            {...register('shippingCents')}
+            onChange={handleChangeDeliveryPrice}
+          />
+
+          <Button
+            type="submit"
+            color="success"
+            disabled={isValid}
+            variant="outlined"
+          >
+            Confirm
+          </Button>
+        </form>
+      </Paper>
+    </div>
   );
 };
