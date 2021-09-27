4af79a037307613bf5865dbc69fdd0423bad211c
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-27 15:55:52 +0200

    fixed url'

--- apps/frontend/src/utils.ts
+++ apps/frontend/src/utils.ts
@@ -1,6 +1,6 @@
 export function validUrl(text: string) {
   const regex = RegExp(
-    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/
+    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)|www\.\w+\..+$/
   );
 
   return !regex.test(text);
