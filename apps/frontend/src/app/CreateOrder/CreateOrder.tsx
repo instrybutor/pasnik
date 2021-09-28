8c88d22c1b2d5c328b8e22114b8ffda9fcb6d8f8
jakubzdanowicz <j.zdanowicz22@gmail.com>
2021-09-28 09:48:48 +0200

    added yup form

--- apps/frontend/src/app/CreateOrder/CreateOrder.tsx
+++ apps/frontend/src/app/CreateOrder/CreateOrder.tsx
@@ -1,23 +1,55 @@
-import React, { ChangeEvent, FC, useCallback, useRef, useState } from 'react';
+import { ChangeEvent, FC, useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 
 import type { CreateOrderDto, OrderModel } from '@pasnik/api/data-transfer';
 import { useOrdersFacade } from '@pasnik/orders-data-access';
 import { Alert, Button, InputAdornment, Paper, TextField } from '@mui/material';
-import { formatter, validUrl } from '../../utils';
+import { formatter } from '../../utils';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    from: yup.string().required(),
+    menuUrl: yup
+      .string()
+      .required()
+      .test('validURL', 'This URL is not valid', (number) => {
+        if (number) {
+          return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)|www\.\w+\..+$/.test(
+            number.toString()
+          );
+        }
+        return false;
+      }),
+    shippingCents: yup
+      .number()
+      .test(
+        'maxDigitsAfterDecimal',
+        'number field must have 2 digits after decimal or less',
+        (number) => {
+          if (number) {
+            return /^\d+(\.\d{1,2})?$/.test(number.toString());
+          }
+          return false;
+        }
+      ),
+  })
+  .required();
 
 export const CreateOrder: FC = () => {
-  const { register, handleSubmit } = useForm();
+  const [deliveryPrice, setDeliveryPrice] = useState<string>();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   const { createOrder } = useOrdersFacade();
-
-  const [restaurant, setRestaurant] = useState<string>('');
-  const [menu, setMenu] = useState<string>('');
-  const [formError, setFormError] = useState<boolean>(false);
-  const [isValidInput, setIsValidInput] = useState<boolean>(false);
-  const [deliveryPrice, setDeliveryPrice] = useState<string>('');
-
-  const deliveryRef = useRef<HTMLInputElement>(null);
 
   const [error, setError] = useState<string | null>(null);
   const history = useHistory();
@@ -25,11 +57,11 @@ export const CreateOrder: FC = () => {
   const onSubmit = (data: CreateOrderDto) => {
     const newPrice = data.shippingCents
       ? +formatter.format(data.shippingCents)
-      : -1;
+      : null;
     createOrder({
       ...data,
       orderAt: new Date().toISOString(),
-      shippingCents: newPrice * 100,
+      shippingCents: newPrice ? newPrice * 100 : undefined,
     })
       .then((params: OrderModel) => {
         history.push(`order/` + params.id);
@@ -37,26 +69,12 @@ export const CreateOrder: FC = () => {
       .catch((err: Error) => setError(err.message));
   };
 
-  const handleChangeRestaurant = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setRestaurant(e.currentTarget.value);
-    },
-    []
-  );
-
-  const handleChangeMenu = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setMenu(e.currentTarget.value);
-    setFormError(validUrl(e.currentTarget.value));
-    setIsValidInput(validUrl(e.currentTarget.value));
-  }, []);
-
   const handleChangeDeliveryPrice = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       setDeliveryPrice(e.currentTarget.value);
     },
     []
   );
-
   return (
     <div>
       <Paper elevation={3} className="formSquare">
@@ -67,34 +85,31 @@ export const CreateOrder: FC = () => {
             autoFocus
             margin="normal"
             label="Restaurant"
-            value={restaurant}
             inputProps={{
               fullWidth: true,
             }}
             {...register('from')}
-            onChange={handleChangeRestaurant}
             type="text"
-            required
           />
+          {errors.from?.message && (
+            <Alert severity="error">{errors.from?.message}</Alert>
+          )}
+
           <TextField
             margin="normal"
-            error={isValidInput}
             label="Menu URL"
-            value={menu}
             inputProps={{
               fullWidth: true,
             }}
             {...register('menuUrl')}
-            onChange={handleChangeMenu}
             type="text"
-            required
           />
-          {isValidInput && (
-            <Alert severity="error">This URL is not valid!</Alert>
+
+          {errors.menuUrl?.message && (
+            <Alert severity="error">{errors.menuUrl?.message}</Alert>
           )}
 
           <TextField
-            inputRef={deliveryRef}
             margin="normal"
             label="Delivery Price (optional)"
             sx={{ m: 1, width: '25ch' }}
@@ -103,18 +118,16 @@ export const CreateOrder: FC = () => {
                 <InputAdornment position="start">PLN</InputAdornment>
               ),
             }}
-            value={deliveryPrice}
             {...register('shippingCents')}
+            value={deliveryPrice}
             onChange={handleChangeDeliveryPrice}
             type="number"
           />
+          {errors.shippingCents?.message && (
+            <Alert severity="error">{errors.shippingCents?.message}</Alert>
+          )}
 
-          <Button
-            type="submit"
-            color="success"
-            disabled={formError}
-            variant="outlined"
-          >
+          <Button type="submit" color="success" variant="outlined">
             Confirm
           </Button>
         </form>
