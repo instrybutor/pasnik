7d17a1aad73e57bf6e48ef579d091528ad0bc7a9
Paweł Kondraciuk <kondraciukpawel@gmail.com>
2021-10-11 17:28:11 +0200

    Remove create order

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -8,7 +8,6 @@ import { PagesDashboard } from '@pasnik/pages/dashboard';
 import { PagesOrder } from '@pasnik/pages/order';
 import { Layout } from '@pasnik/layout';
 import { PagesOrders } from '@pasnik/pages/orders';
-import { PagesCreateOrder } from '@pasnik/pages/create-order';
 
 export function App() {
   const version = process.env.NX_COMMIT_HASH;
@@ -21,7 +20,6 @@ export function App() {
 
           <Layout version={version}>
             <PrivateRoute exact path="/" component={PagesDashboard} />
-            <PrivateRoute path="/create-order" component={PagesCreateOrder} />
             <PrivateRoute path="/history" component={PagesOrders} />
             <PrivateRoute path="/order/:orderId" component={PagesOrder} />
           </Layout>
