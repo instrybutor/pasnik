3ad78f7a1f54553f076e1dec3edb8dd23f0d4139
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2021-10-12 13:47:15 +0200

    Fix deps

--- libs/pages/order/src/lib/order-store/order.facade.tsx
+++ libs/pages/order/src/lib/order-store/order.facade.tsx
@@ -11,10 +11,11 @@ import { useCallback } from 'react';
 
 export const useOrderFacade = () => {
   const store = useOrderStore();
+  const orderId = useOrderStore((store) => store.order?.id);
 
   const resetStore = useCallback(() => {
     store.reset();
-  }, [store]);
+  }, [store.reset]);
 
   const addDish = useCallback(
     async (orderId: string, addDishDto: AddDishDto) => {
@@ -23,7 +24,7 @@ export const useOrderFacade = () => {
 
       return dish;
     },
-    [store]
+    [store.addDish]
   );
 
   const fetchDishes = useCallback(
@@ -33,7 +34,7 @@ export const useOrderFacade = () => {
 
       return dishes;
     },
-    [store]
+    [store.setDishes]
   );
 
   const fetchOrder = useCallback(
@@ -43,61 +44,61 @@ export const useOrderFacade = () => {
 
       return order;
     },
-    [store]
+    [store.setOrder]
   );
 
   const markAsClosed = useCallback(async (): Promise<OrderModel> => {
-    const order = await service.markAsClosed(store.order!.id);
+    const order = await service.markAsClosed(orderId!);
     store.setOrder(order);
 
     return order;
-  }, [store]);
+  }, [store.setOrder, orderId]);
 
   const markAsOpen = useCallback(async (): Promise<OrderModel> => {
-    const order = await service.markAsOpen(store.order!.id);
+    const order = await service.markAsOpen(orderId!);
     store.setOrder(order);
 
     return order;
-  }, [store]);
+  }, [store.setOrder, orderId]);
 
   const markAsOrdered = useCallback(async (): Promise<OrderModel> => {
-    const order = await service.markAsOrdered(store.order!.id);
+    const order = await service.markAsOrdered(orderId!);
     store.setOrder(order);
 
     return order;
-  }, [store]);
+  }, [store.setOrder, orderId]);
 
   const markAsPaid = useCallback(
     async (payer: UserModel): Promise<OrderModel> => {
-      const order = await service.markAsPaid(store.order!.id, payer.id);
+      const order = await service.markAsPaid(orderId!, payer.id);
       store.setOrder(order);
 
       return order;
     },
-    [store]
+    [store.setOrder, orderId]
   );
 
   const markAsDelivered = useCallback(async (): Promise<OrderModel> => {
-    const order = await service.markAsDelivered(store.order!.id);
+    const order = await service.markAsDelivered(orderId!);
     store.setOrder(order);
 
     return order;
-  }, [store]);
+  }, [store.setOrder, orderId]);
 
   const deleteDish = useCallback(
     async (dish: DishModel): Promise<void> => {
-      await service.deleteDish(store.order!.id, dish.id);
+      await service.deleteDish(orderId!, dish.id);
       store.deleteDish(dish);
     },
-    [store]
+    [store.deleteDish, orderId]
   );
 
   const updateDish = useCallback(
     async (dishId, dto: AddDishDto): Promise<void> => {
-      const dish = await service.updateDish(store.order!.id, dishId, dto);
+      const dish = await service.updateDish(orderId!, dishId, dto);
       store.updateDish(dish);
     },
-    [store]
+    [store.updateDish, orderId]
   );
 
   return {
--- libs/pages/order/src/lib/order-summary/order-summary.hook.tsx
+++ libs/pages/order/src/lib/order-summary/order-summary.hook.tsx
@@ -1,6 +1,6 @@
 import { DishModel, OrderModel, UserModel } from '@pasnik/api/data-transfer';
 import { useAuth } from '@pasnik/shared/utils-auth';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export interface UserDishesSummary {
   user: UserModel;
@@ -17,47 +17,60 @@ export const useOrderSummary = (order: OrderModel) => {
   const [currentUserSummary, setCurrentUserSummary] =
     useState<UserDishesSummary | null>(null);
 
-  const applyShipping = (summaries: UserDishesSummary[]) => {
-    summaries.forEach((userSummary) => {
-      userSummary.shipping += order.shippingCents! / summaries.length;
-    });
-  };
-
-  const groupSummaries = (dishes: DishModel[]) => {
-    const summaries = dishes.reduce((acc, dish) => {
-      dish.usersDishes.forEach(({ user }) => {
-        const userSummary: UserDishesSummary = acc[user.id] || {
-          user,
-          dishes: [],
-          total: 0,
-          shipping: 0,
-        };
-        userSummary.dishes.push(dish);
-        userSummary.total += dish.priceCents;
-        acc[user.id] = userSummary;
+  const applyShipping = useCallback(
+    (summaries: UserDishesSummary[]) => {
+      summaries.forEach((userSummary) => {
+        userSummary.shipping += order.shippingCents! / summaries.length;
       });
-      return acc;
-    }, {} as Record<UserModel['id'], UserDishesSummary>);
-    if (order.shippingCents) {
-      applyShipping(Object.values(summaries));
-    }
-    return {
-      currentUserSummary: summaries[user!.id],
-      userDishesSummaries: Object.values(summaries),
-    };
-  };
+    },
+    [order]
+  );
 
-  const sortSummaries = (userDishesSummaries: UserDishesSummary[]) => {
-    return userDishesSummaries.sort((summary) =>
-      summary.user.id === user!.id ? 0 : 1
-    );
-  };
+  const groupSummaries = useCallback(
+    (dishes: DishModel[]) => {
+      const summaries = dishes.reduce((acc, dish) => {
+        dish.usersDishes.forEach(({ user }) => {
+          const userSummary: UserDishesSummary = acc[user.id] || {
+            user,
+            dishes: [],
+            total: 0,
+            shipping: 0,
+          };
+          userSummary.dishes.push(dish);
+          userSummary.total += dish.priceCents;
+          acc[user.id] = userSummary;
+        });
+        return acc;
+      }, {} as Record<UserModel['id'], UserDishesSummary>);
+      if (order.shippingCents) {
+        applyShipping(Object.values(summaries));
+      }
+      return {
+        currentUserSummary: summaries[user!.id],
+        userDishesSummaries: Object.values(summaries),
+      };
+    },
+    [applyShipping, order, user]
+  );
 
-  const setDishes = (dishes: DishModel[]) => {
-    const { userDishesSummaries, currentUserSummary } = groupSummaries(dishes);
-    setGrouppedSummaries(sortSummaries(userDishesSummaries));
-    setCurrentUserSummary(currentUserSummary);
-  };
+  const sortSummaries = useCallback(
+    (userDishesSummaries: UserDishesSummary[]) => {
+      return userDishesSummaries.sort((summary) =>
+        summary.user.id === user!.id ? 0 : 1
+      );
+    },
+    [user]
+  );
+
+  const setDishes = useCallback(
+    (dishes: DishModel[]) => {
+      const { userDishesSummaries, currentUserSummary } =
+        groupSummaries(dishes);
+      setGrouppedSummaries(sortSummaries(userDishesSummaries));
+      setCurrentUserSummary(currentUserSummary);
+    },
+    [groupSummaries, setGrouppedSummaries, setCurrentUserSummary]
+  );
 
   return {
     grouppedSummaries,
