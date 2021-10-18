c346fd1547afcdc9927cb79793a54d522ce0368a
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2021-10-18 17:14:22 +0200

    Remove comments

--- apps/nestjs-api/src/app/db/db.config.ts
+++ apps/nestjs-api/src/app/db/db.config.ts
@@ -19,13 +19,8 @@ export const DatabaseConfig: ConnectionOptions = {
   // We are using migrations, synchronize should be set to false.
   synchronize: false,
 
-  // Run migrations automatically,
-  // you can disable this if you prefer running migration manually.
   migrationsRun: true,
 
-  // allow both start:prod and start:dev to use migrations
-  // __dirname is either dist or src folder, meaning either
-  // the compiled js in prod or the ts in dev
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
     migrationsDir: __dirname + '/migrations',
