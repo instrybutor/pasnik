c4680eb81f7627dea65e0264e57a7327960a15de
Lukasz Szymanski <lukasz.szymansky@gmail.com>
2021-10-19 15:50:13 +0200

    fix/PAS-50 fix shipping cents

--- libs/components/src/lib/price/price.tsx
+++ libs/components/src/lib/price/price.tsx
@@ -1,14 +1,16 @@
+import currency from 'currency.js';
 export interface PriceProps {
   priceCents?: number | string | null;
 }
 
-const currencyFormatter = Intl.NumberFormat('pl-pl', {
-  currency: 'PLN',
-  style: 'currency',
-});
-
 export function Price({ priceCents }: PriceProps) {
-  return (
-    <>{priceCents ? currencyFormatter.format(+priceCents / 100) : '0 zł'}</>
-  );
+  const price = currency(priceCents ?? 0, {
+    fromCents: true,
+    pattern: '# !',
+  }).format({
+    precision: 2,
+    symbol: 'PLN',
+  });
+
+  return <span>{price}</span>;
 }
