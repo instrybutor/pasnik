13cc997865153bdbd19ec6cce6564bda592a4c2d
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2021-10-23 22:56:13 +0200

    Remove devtools

--- libs/pages/dashboard/src/lib/dashboard-store/dashboard.store.tsx
+++ libs/pages/dashboard/src/lib/dashboard-store/dashboard.store.tsx
@@ -1,5 +1,4 @@
 import create from 'zustand';
-import { devtools } from 'zustand/middleware';
 
 import type { OrderModel } from '@pasnik/api/data-transfer';
 
@@ -11,28 +10,26 @@ interface DashboardStore {
   setOrders: (orders: OrderModel[]) => void;
 }
 
-export const useDashboardStore = create<DashboardStore>(
-  devtools((set) => ({
-    entities: {},
-    ids: [],
-    balance: 0,
-    orders: 0,
-    transfers: 0,
-    isLoading: true,
+export const useDashboardStore = create<DashboardStore>((set) => ({
+  entities: {},
+  ids: [],
+  balance: 0,
+  orders: 0,
+  transfers: 0,
+  isLoading: true,
 
-    setOrders: (orders: OrderModel[]) => {
-      set((state) => ({
-        ...state,
-        isLoading: false,
-        ids: orders.map((order) => order.id),
-        entities: orders.reduce(
-          (collection, order) => ({
-            ...collection,
-            [order.id]: order,
-          }),
-          {}
-        ),
-      }));
-    },
-  }))
-);
+  setOrders: (orders: OrderModel[]) => {
+    set((state) => ({
+      ...state,
+      isLoading: false,
+      ids: orders.map((order) => order.id),
+      entities: orders.reduce(
+        (collection, order) => ({
+          ...collection,
+          [order.id]: order,
+        }),
+        {}
+      ),
+    }));
+  },
+}));
