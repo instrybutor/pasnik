da4d7a5d7ad440be22955b8265ec9567372ba112
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2022-07-01 12:49:03 +0200

    Fix slugs

--- libs/features/orders/src/lib/orders-empty/orders-empty.tsx
+++ libs/features/orders/src/lib/orders-empty/orders-empty.tsx
@@ -2,8 +2,13 @@ import { useTranslation } from 'react-i18next';
 import { BeakerIcon } from '@heroicons/react/outline';
 import { CreateOrderModal } from '../create-order-modal/create-order-modal';
 import { ModalButton } from '@pasnik/components';
+import { WorkspaceModel } from '@pasnik/api/data-transfer';
 
-export function OrdersEmpty() {
+export interface OrdersEmptyProps {
+  workspace?: WorkspaceModel;
+}
+
+export function OrdersEmpty({ workspace }: OrdersEmptyProps) {
   const { t } = useTranslation();
   return (
     <div className="text-center bg-white px-4 py-12 flex flex-col items-center">
@@ -16,12 +21,15 @@ export function OrdersEmpty() {
           {t('dashboard.starting_info')}
         </span>
       </div>
-      <ModalButton
-        className="bg-cyan-500 hover:bg-cyan-600 text-white py-2 text-sm rounded px-10 mt-4"
-        modal={CreateOrderModal}
-      >
-        {t('dashboard.create_order')}
-      </ModalButton>
+      {workspace && (
+        <ModalButton
+          className="bg-cyan-500 hover:bg-cyan-600 text-white py-2 text-sm rounded px-10 mt-4"
+          modal={CreateOrderModal}
+          props={{ workspace }}
+        >
+          {t('dashboard.create_order')}
+        </ModalButton>
+      )}
     </div>
   );
 }
