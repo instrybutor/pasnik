1844cb38eca7040013c22e568bcff7b28696afe1
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2022-07-01 12:09:26 +0200

    Remove unused packages (#97)

--- libs/components/src/lib/select/select.tsx
+++ libs/components/src/lib/select/select.tsx
@@ -1,8 +1,8 @@
-import React, { Fragment, PropsWithChildren } from 'react';
-import { Listbox, Portal, Transition } from '@headlessui/react';
-import { usePopper } from 'react-popper';
+import React, { PropsWithChildren } from 'react';
+import { Listbox } from '@headlessui/react';
 import classNames from 'classnames';
 import { CheckIcon } from '@heroicons/react/outline';
+import { Float } from '@headlessui-float/react';
 
 export interface Props<T> {
   items: T[];
@@ -28,23 +28,6 @@ export const Select = <T,>({
   children,
   renderItem,
 }: PropsWithChildren<Props<T>>) => {
-  const popperElRef = React.useRef<HTMLDivElement>(null);
-  const [targetElement, setTargetElement] = React.useState<HTMLElement | null>(
-    null
-  );
-  const [popperElement, setPopperElement] =
-    React.useState<HTMLDivElement | null>(null);
-
-  const { styles, attributes } = usePopper(targetElement, popperElement, {
-    placement: 'bottom-start',
-  });
-
-  const onOpen = React.useCallback(
-    () => setPopperElement(popperElRef.current),
-    []
-  );
-
-  const onClose = React.useCallback(() => setPopperElement(null), []);
   const isElementSelected = React.useCallback(
     (item: T) =>
       selectedItem
@@ -55,9 +38,14 @@ export const Select = <T,>({
 
   return (
     <Listbox value={selectedItem} onChange={onSelect}>
-      <div className="relative">
+      <Float
+        placement="bottom-start"
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        portal
+      >
         <Listbox.Button
-          ref={setTargetElement}
           className={classNames(
             'relative bg-white text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-cyan-500 focus:border-cyan-500 rounded-full w-8'
           )}
@@ -65,50 +53,37 @@ export const Select = <T,>({
           {children}
         </Listbox.Button>
 
-        <Portal>
-          <div ref={popperElRef} style={styles.popper} {...attributes.popper}>
-            <Transition
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-              beforeEnter={onOpen}
-              afterLeave={onClose}
+        <Listbox.Options className="flex flex-col absolute z-10 mt-1 bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+          {items.map((item, index) => (
+            <Listbox.Option
+              key={keyExtraction ? keyExtraction(item) : index}
+              className={({ active }) =>
+                classNames(
+                  active ? 'text-white bg-cyan-600' : 'text-gray-900',
+                  'cursor-default select-none relative py-2 px-3 w-60 flex'
+                )
+              }
+              value={item}
             >
-              <Listbox.Options className="flex flex-col absolute z-10 mt-1 bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                {items.map((item, index) => (
-                  <Listbox.Option
-                    key={keyExtraction ? keyExtraction(item) : index}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'text-white bg-cyan-600' : 'text-gray-900',
-                        'cursor-default select-none relative py-2 px-3 w-60 flex'
-                      )
-                    }
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex flex-1">
-                          {renderItem({ selected, active, item })}
-                        </div>
-                        {isElementSelected(item) ? (
-                          <CheckIcon
-                            className={classNames(
-                              'w-6 h-6 ',
-                              active ? 'text-white' : 'text-slate-600'
-                            )}
-                          />
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </Portal>
-      </div>
+              {({ selected, active }) => (
+                <>
+                  <div className="flex flex-1">
+                    {renderItem({ selected, active, item })}
+                  </div>
+                  {isElementSelected(item) ? (
+                    <CheckIcon
+                      className={classNames(
+                        'w-6 h-6 ',
+                        active ? 'text-white' : 'text-slate-600'
+                      )}
+                    />
+                  ) : null}
+                </>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Float>
     </Listbox>
   );
 };
